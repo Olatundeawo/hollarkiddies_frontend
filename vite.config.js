e982c72c188e373a4efdb1d45027802499c16c77
Your Name <you@example.com>
2024-11-06 14:10:09 -0800

    modify

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://hollarkiddies-backend.onrender.com',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/images': {
         target: 'https://hollarkiddies-backend.onrender.com',
