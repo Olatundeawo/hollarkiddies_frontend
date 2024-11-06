a27dd44f080e7d76b348d8faccbd69f427da67d1
Your Name <you@example.com>
2024-11-06 14:13:48 -0800

    modify

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     proxy: {
       '/api': {
         target: 'https://hollarkiddies-backend.onrender.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/images': {
         target: 'https://hollarkiddies-backend.onrender.com',
