df2ef5241979c71334afea2a074a0cf5fc6952ed
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-08-14 20:34:59 +0100

    fix while the update features is not working and added a font family to it

--- src/controllers/product_details.jsx
+++ src/controllers/product_details.jsx
@@ -2,52 +2,72 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { MutatingDots } from "react-loader-spinner";
 
 const ProductDetail = () => {
   const { id } = useParams();
   const [products, setProducts] = useState(null);
   const [activeImage, setActiveImage] = useState(null); // State to store the active image
-  const navigate = useNavigate()
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleClick = (id) => {
-    navigate(`/product/${id}/update`)
-  }
+    navigate(`/product/${id}/update`);
+  };
 
   const handleDeleteClick = (id) => {
-    if (confirm('Are you sure You want to delete this item?'))
-    {
-
-      navigate(`/product/${id}/delete`)
+    if (confirm("Are you sure You want to delete this item?")) {
+      navigate(`/product/${id}/delete`);
     }
-  }
+  };
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
-    setIsAuthenticated(!!token)
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
   }, []);
 
   useEffect(() => {
-    axios.get(`https://hollarkiddies-backend.onrender.com/api/product/${id}`).then((response) => {
-      const productData = response.data;
-      setProducts(productData);
-      console.log(response.data)
+    axios
+      .get(`https://hollarkiddies-backend.onrender.com/api/product/${id}`)
+      .then((response) => {
+        const productData = response.data;
+        setProducts(productData);
+        console.log(response.data);
 
-      // Set the first image as the active image by default
-      if (productData.length > 0 && productData[0].images && productData[0].images.length > 0) {
-        setActiveImage(productData[0].images[0].image_path);
-      }
-    });
+        // Set the first image as the active image by default
+        if (
+          productData.length > 0 &&
+          productData[0].images &&
+          productData[0].images.length > 0
+        ) {
+          setActiveImage(productData[0].images[0].image_path);
+        }
+      });
   }, [id]);
 
   if (!products) {
-    return <p className="text-center text-lg font-semibold">Loading...</p>;
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-50">
+        <MutatingDots
+          height="100"
+          width="100"
+          color="#4f46e5"
+          secondaryColor="#6366f1"
+          radius="12.5"
+          ariaLabel="mutating-dots-loading"
+          visible={true}
+        />
+      </div>
+    );
   }
 
   return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="font-libertinus max-w-6xl mx-auto p-6">
       {products.map((product) => (
-        <div key={product.product_id} className="flex flex-col md:flex-row bg-white shadow-lg rounded-lg overflow-hidden mb-6">
+        <div
+          key={product.product_id}
+          className="flex flex-col md:flex-row bg-white shadow-lg rounded-lg overflow-hidden mb-6"
+        >
           {/* Left Column: Product Images */}
           <div className="md:w-1/2 p-4">
             <div className="mb-4">
@@ -58,7 +78,9 @@ const ProductDetail = () => {
                   className="w-full h-auto object-cover rounded-lg"
                 />
               ) : (
-                <p className="text-gray-500 text-center">No images available.</p>
+                <p className="text-gray-500 text-center">
+                  No images available.
+                </p>
               )}
             </div>
 
@@ -70,13 +92,14 @@ const ProductDetail = () => {
                       key={index}
                       src={image.image_path}
                       alt={`Thumbnail ${index + 1}`}
-                      className={`w-20 h-20 object-cover border border-gray-300 rounded-lg cursor-pointer ${activeImage === image ? 'border-blue-500' : ''}`} // Add border to the active thumbnail
+                      className={`w-20 h-20 object-cover border border-gray-300 rounded-lg cursor-pointer ${
+                        activeImage === image ? "border-blue-500" : ""
+                      }`} // Add border to the active thumbnail
                       onClick={() => setActiveImage(image.image_path)} // Set the clicked thumbnail as the active image
                     />
                   ))
                 : null}
             </div>
-            {console.log(product.images.map((image) => image.image_path))}
           </div>
 
           {/* Right Column: Product Details */}
@@ -84,12 +107,18 @@ const ProductDetail = () => {
             <h1 className="text-3xl font-bold text-gray-800">{product.name}</h1>
 
             {/* Price */}
-            <div className="text-2xl font-bold text-green-500 mt-4">₦{product.price}</div>
+            <div className="text-2xl font-bold text-green-500 mt-4">
+              ₦{product.price}
+            </div>
 
             {/* Description */}
             <div className="mt-6">
-              <h3 className="text-lg font-semibold text-gray-700">Description</h3>
-              <p className="text-gray-600 mt-2">{product.description}</p>
+              <h3 className="text-lg font-semibold text-gray-700">
+                Description
+              </h3>
+              <p className="font-clash text-gray-600 mt-2">
+                {product.description}
+              </p>
             </div>
 
             {/* Creation Date */}
@@ -107,14 +136,18 @@ const ProductDetail = () => {
               </button>
               {isAuthenticated && (
                 <>
-              <button className="bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700"
-              onClick={() => handleClick(product.product_id)}>
-                Edit
-              </button>
-              <button className="bg-red-300 text-white px-6 py-3 rounded-md hover:bg-red-700"
-              onClick={() => handleDeleteClick(product.product_id)}>
-                Delete
-              </button>
+                  <button
+                    className="bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700"
+                    onClick={() => handleClick(product.product_id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="bg-red-300 text-white px-6 py-3 rounded-md hover:bg-red-700"
+                    onClick={() => handleDeleteClick(product.product_id)}
+                  >
+                    Delete
+                  </button>
                 </>
               )}
             </div>
