1d16b7665d424cc81dd71755380bdf2e656a8649
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-08-14 20:31:14 +0100

    add the font i want to use

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
     "./assest/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        libertinus: ["Libertinus Serif", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        satoshi: ['Satoshi', 'sans-serif'],
+        clash: ['Clash Display', 'sans-serif'],
+        dancing: ['Dancing Script', 'cursive'],
+        lobster: ['Lobster Two', 'cursive'],
+        fjalla: ['Fjalla One', 'sans-serif'],
+        pacifico: ['Pacifico', 'cursive'],
+        asap: ['Asap', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
